86722e76e5152bda2b4e86e7b88cb11019d6ace0
pandiaaman <pandiaaman16@gmail.com>
2023-02-21 13:49:55 +0530

    adding the user index page and otp

--- userindex.php
+++ userindex.php
@@ -2,4 +2,6 @@
 //this page contains the home page of the user
 //show all the details of the user here
 //there will be a logout button that logs the user out and lands them back to the index page of the website
+session_start();
+
 ?>
